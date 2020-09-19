6234287377bb0832780cf11a332c118528614ec9
Raphaël Bonnet <harcyah@gmail.com>
2020-09-19 23:30:12 +0200

    Update tracked achievements

--- HeroInterface.lua
+++ HeroInterface.lua
@@ -38,13 +38,12 @@ TOYS[127659] = 47; -- chapeau-de-boucanier-de-fer-fantomatique
 TOYS[156833] = 48; -- siffletimbre-de-katy
 
 local TRACKED_ACHIEVEMENTS = {
-	6943,
 	13128,
 	11573,
 	9643,
+	8101,
 	13638,
 	12104,
-	10603,
 	12026
 }
 
